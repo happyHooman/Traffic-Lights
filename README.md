61a31ac4f4249f375cf30d17290257641ad988db
HappyHooman <tornea.alexandru@gmail.com>
2016-06-22 18:12:29 +0300

    minor updates some fixes

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Traffic-Lights
 
 How to use?
-  -  download project
+  - download project
   - open project folder
   - open CMD in the project folder
   - type next commands(must have node installed):
+  
   -- npm install
+  
   -- npm run dev
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,10 +4,16 @@ import React from "react";
 import Semaphore from "./Semaphore";
 
 export default class Layout extends React.Component {
+
     render() {
+        const action = 'Start';
         return (
             <div>
+                <label>Click on Semaphore to start.</label>
                 <Semaphore />
+                <div>
+                    <button>{action}</button>
+                </div>
             </div>
         );
     }
